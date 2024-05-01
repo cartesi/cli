sunodo/sunodo

feat(cli): migrate from oclif to clipanion

apps/cli/src/commands/create.ts
@@ -1,5 +1,5 @@
-import { Args, Command, Flags } from "@oclif/core";
 import chalk from "chalk";
+import { Command, Option } from "clipanion";
 import type { TemplateProvider } from "giget";
 import { DownloadTemplateResult, downloadTemplate } from "giget";
 import ora from "ora";
@@ -7,38 +7,24 @@ import ora from "ora";
 const DEFAULT_TEMPLATES_BRANCH = "sdk-0.3";
 
 export default class CreateCommand extends Command {
-    static description = "Create application";
+    static paths = [["create"]];
+
+    static usage = Command.Usage({
+        description: "Create application",
+    });
 
     static examples = ["<%= config.bin %> <%= command.id %>"];
 
-    static args = {
-        name: Args.string({
-            description: "application and directory name",
-            required: true,
-        }),
-    };
+    name = Option.String();
 
-    static flags = {
-        template: Flags.string({
-            description: "template name to use",
-            required: true,
-            options: [
-                "cpp",
-                "cpp-low-level",
-                "go",
-                "javascript",
-                "lua",
-                "python",
-                "ruby",
-                "rust",
-                "typescript",
-            ],
-        }),
-        branch: Flags.string({
-            description: `sunodo/sunodo-templates repository branch name to use`,
-            default: DEFAULT_TEMPLATES_BRANCH,
-        }),
-    };
+    template = Option.String("--template", {
+        description: "template name to use",
+        required: true,
+    });
+
+    branch = Option.String("--branch", {
+        description: `sunodo/sunodo-templates repository branch name to use`,
+    });
 
     private async download(
         template: string,
@@ -61,14 +47,13 @@ export default class CreateCommand extends Command {
         });
     }
 
-    public async run(): Promise<void> {
-        const { args, flags } = await this.parse(CreateCommand);
+    public async execute(): Promise<void> {
         const spinner = ora("Creating application...").start();
         try {
             const { dir } = await this.download(
-                flags.template,
-                flags.branch,
-                args.name,
+                this.template,
+                this.branch || DEFAULT_TEMPLATES_BRANCH,
+                this.name,
             );
             spinner.succeed(`Application created at ${chalk.cyan(dir)}`);
         } catch (e: unknown) {

apps/cli/src/commands/deploy/index.ts
@@ -1,41 +1,36 @@
 import confirm from "@inquirer/confirm";
 import select from "@inquirer/select";
-import { Flags } from "@oclif/core";
 import chalk from "chalk";
+import { Command, Option, UsageError } from "clipanion";
 import open, { apps } from "open";
-import { URL } from "url";
+import * as t from "typanion";
 
 import { SunodoCommand } from "../../sunodoCommand.js";
 
-export default class Deploy extends SunodoCommand<typeof Deploy> {
-    static summary = "Deploy application to a live network.";
+export default class Deploy extends SunodoCommand {
+    static paths = [["deploy"]];
 
-    static description =
-        "Package and deploy the application to a supported live network.";
+    static usage = Command.Usage({
+        description: "Deploy application to a live network.",
+        details:
+            "Package and deploy the application to a supported live network.",
+    });
 
-    static examples = ["<%= config.bin %> <%= command.id %>"];
+    hosting = Option.String<"self-hosted" | "third-party">("--hosting", {
+        validator: t.isEnum(["self-hosted", "third-party"]),
+        description: 'hosting type ("self-hosted", "third-party")',
+    });
 
-    static flags = {
-        hosting: Flags.string({
-            options: ["self-hosted", "third-party"] as const,
-            summary: "hosting type",
-            description:
-                "Select wheather the user will host an application node himself, or use a third-party node provider",
-        }),
-        webapp: Flags.url({
-            description: "address of sunodo webapp",
-            default: new URL("https://sunodo.io/deploy"),
-            hidden: true,
-        }),
-    };
+    webapp = Option.String("--webapp", {
+        description: "address of sunodo webapp",
+        hidden: true,
+    });
 
-    public async run(): Promise<void> {
-        const { flags } = await this.parse(Deploy);
-
+    public async execute(): Promise<void> {
         // print machine hash
         const templateHash = this.getMachineHash();
         if (!templateHash) {
-            this.error(
+            throw new UsageError(
                 "Cartesi machine snapshot not found, run 'sunodo build'",
             );
         }
@@ -46,7 +41,7 @@ export default class Deploy extends SunodoCommand<typeof Deploy> {
 
         // ask for deployment type
         const hosting =
-            flags.hosting ||
+            this.hosting ||
             (await select<"self-hosted" | "third-party">({
                 message: "Select hosting type",
                 choices: [
@@ -76,18 +71,20 @@ You will need the following infrastructure:
         switch (hosting) {
             case "self-hosted": {
                 // build docker image
-                await this.config.runCommand("deploy:build");
+                await this.cli.run(["deploy", "build"]);
 
                 queryString = `?templateHash=${templateHash}`;
                 break;
             }
             case "third-party": {
-                this.error("Third-party provider deployment not supported yet");
+                throw new UsageError(
+                    "Third-party provider deployment not supported yet",
+                );
             }
         }
 
         // prompt user to open webapp for onchain deployment
-        const deployUrl = `${flags.webapp}${queryString}`;
+        const deployUrl = `${this.webapp ?? "https://sunodo.io/deploy"}${queryString}`;
         if (await confirm({ message: `Open ${chalk.cyan(deployUrl)}?` })) {
             open(deployUrl, { app: { name: apps.chrome } });
         }

apps/cli/src/commands/hash.ts
@@ -1,27 +1,29 @@
 import chalk from "chalk";
+import { Command, UsageError } from "clipanion";
 
 import { SunodoCommand } from "../sunodoCommand.js";
 
-export default class HashCommand extends SunodoCommand<typeof HashCommand> {
-    static summary = "Prints out image hash generated by the build command";
+export default class HashCommand extends SunodoCommand {
+    static paths = [["hash"]];
 
-    static description =
-        "Converts the binary generated by the build command to hexadecimal and prints out the result to console";
+    static usage = Command.Usage({
+        description: "Prints out image hash generated by the build command",
+        details:
+            "Converts the binary generated by the build command to hexadecimal and prints out the result to console",
+    });
 
-    public static enableJsonFlag = true;
-
-    public async run() {
+    public async execute() {
         const hash = this.getMachineHash();
         if (hash) {
-            if (!this.jsonEnabled()) {
-                process.stdout.write(
+            if (!this.jsonEnabled) {
+                this.context.stdout.write(
                     `${chalk.green("?")} Cartesi machine templateHash ${chalk.cyan(hash)}\n`,
                 );
             } else {
-                return { hash };
+                this.context.stdout.write(JSON.stringify({ hash }));
             }
         } else {
-            this.error(
+            throw new UsageError(
                 "Cartesi machine snapshot not found, run 'sunodo build'",
             );
         }

apps/cli/src/commands/send/dapp-address.ts
@@ -1,3 +1,4 @@
+import { Command } from "clipanion";
 import { Address, PublicClient, WalletClient } from "viem";
 
 import {
@@ -6,13 +7,13 @@ import {
 } from "../../contracts.js";
 import { SendBaseCommand } from "./index.js";
 
-export default class SendAddress extends SendBaseCommand<typeof SendAddress> {
-    static summary = "Send DApp address input to the application.";
+export default class SendAddress extends SendBaseCommand {
+    static paths = [["send", "dapp-address"]];
 
-    static description =
-        "Sends an input to the application with its own address.";
-
-    static examples = ["<%= config.bin %> <%= command.id %>"];
+    static usage = Command.Usage({
+        description: "Send DApp address input to the application.",
+        details: "Sends an input to the application with its own address.",
+    });
 
     public async send(
         publicClient: PublicClient,

apps/cli/src/index.ts
@@ -1,1 +1,41 @@
-export { run } from "@oclif/core";
+import { Builtins, Cli } from "clipanion";
+
+import pkg from "../package.json";
+import AddressBookCommand from "./commands/address-book.js";
+import BuildCommand from "./commands/build.js";
+import CleanCommand from "./commands/clean.js";
+import CreateCommand from "./commands/create.js";
+import DeployBuildCommand from "./commands/deploy/build.js";
+import DeployCommand from "./commands/deploy/index.js";
+import DoctorCommand from "./commands/doctor.js";
+import HashCommand from "./commands/hash.js";
+import RunCommand from "./commands/run.js";
+import SendAddressCommand from "./commands/send/dapp-address.js";
+import SendCommand from "./commands/send/index.js";
+import ShellCommand from "./commands/shell.js";
+
+const [, , ...args] = process.argv;
+const cli = new Cli({
+    binaryName: "sunodo",
+    binaryLabel: "Sunodo",
+    binaryVersion: pkg.version,
+});
+
+cli.register(Builtins.DefinitionsCommand);
+cli.register(Builtins.HelpCommand);
+cli.register(Builtins.TokensCommand);
+cli.register(Builtins.VersionCommand);
+
+cli.register(AddressBookCommand);
+cli.register(BuildCommand);
+cli.register(CleanCommand);
+cli.register(CreateCommand);
+cli.register(DeployCommand);
+cli.register(DeployBuildCommand);
+cli.register(DoctorCommand);
+cli.register(HashCommand);
+cli.register(RunCommand);
+cli.register(SendCommand);
+cli.register(SendAddressCommand);
+cli.register(ShellCommand);
+cli.runExit(args);

cartesi/cli

apps/cli/src/commands/create.ts
@@ -1,45 +1,32 @@
-import { Args, Flags } from "@oclif/core";
 import chalk from "chalk";
+import { Command, Option } from "clipanion";
 import type { TemplateProvider } from "giget";
 import { DownloadTemplateResult, downloadTemplate } from "giget";
 import ora from "ora";
+
 import { BaseCommand } from "../baseCommand.js";
 
 export const DEFAULT_TEMPLATES_BRANCH = "sdk-0.6";
 
-export default class CreateCommand extends BaseCommand<typeof CreateCommand> {
-    static description = "Create application";
+export default class CreateCommand extends BaseCommand {
+    static paths = [["create"]];
+
+    static usage = Command.Usage({
+        description: "Create application",
+    });
 
     static examples = ["<%= config.bin %> <%= command.id %>"];
 
-    static args = {
-        name: Args.string({
-            description: "application and directory name",
-            required: true,
-        }),
-    };
+    name = Option.String();
 
-    static flags = {
-        template: Flags.string({
-            description: "template name to use",
-            required: true,
-            options: [
-                "cpp",
-                "cpp-low-level",
-                "go",
-                "javascript",
-                "lua",
-                "python",
-                "ruby",
-                "rust",
-                "typescript",
-            ],
-        }),
-        branch: Flags.string({
-            description: `cartesi/application-templates repository branch name to use`,
-            default: DEFAULT_TEMPLATES_BRANCH,
-        }),
-    };
+    template = Option.String("--template", {
+        description: "template name to use",
+        required: true,
+    });
+
+    branch = Option.String("--branch", {
+        description: `cartesi/application-templates repository branch name to use`,
+    });
 
     private async download(
         template: string,
@@ -62,14 +49,13 @@ export default class CreateCommand extends BaseCommand<typeof CreateCommand> {
         });
     }
 
-    public async run(): Promise<void> {
-        const { args, flags } = await this.parse(CreateCommand);
+    public async execute(): Promise<void> {
         const spinner = ora("Creating application...").start();
         try {
             const { dir } = await this.download(
-                flags.template,
-                flags.branch,
-                args.name,
+                this.template,
+                this.branch || DEFAULT_TEMPLATES_BRANCH,
+                this.name,
             );
             spinner.succeed(`Application created at ${chalk.cyan(dir)}`);
         } catch (e: unknown) {

apps/cli/src/commands/deploy/index.ts
@@ -1,40 +1,37 @@
 import confirm from "@inquirer/confirm";
 import select from "@inquirer/select";
-import { Flags } from "@oclif/core";
 import chalk from "chalk";
+import { Command, Option, UsageError } from "clipanion";
 import open, { apps } from "open";
+import * as t from "typanion";
 
 import { BaseCommand } from "../../baseCommand.js";
 
-export default class Deploy extends BaseCommand<typeof Deploy> {
-    static summary = "Deploy application to a live network.";
+export default class DeployCommand extends BaseCommand {
+    static paths = [["deploy"]];
 
-    static description =
-        "Package and deploy the application to a supported live network.";
+    static usage = Command.Usage({
+        description: "Deploy application to a live network.",
+        details:
+            "Package and deploy the application to a supported live network.",
+    });
 
-    static examples = ["<%= config.bin %> <%= command.id %>"];
+    hosting = Option.String<"self-hosted" | "third-party">("--hosting", {
+        validator: t.isEnum(["self-hosted", "third-party"]),
+        description: 'hosting type ("self-hosted", "third-party")',
+    });
 
-    static flags = {
-        hosting: Flags.string({
-            options: ["self-hosted", "third-party"] as const,
-            summary: "hosting type",
-            description:
-                "Select wheather the user will host an application node himself, or use a third-party node provider",
-        }),
-        webapp: Flags.url({
-            description: "address of deploy webapp",
-            required: true,
-        }),
-    };
+    webapp = Option.String("--webapp", {
+        description: "address of deploy webapp",
+        hidden: true,
+    });
 
-    public async run(): Promise<void> {
-        const { flags } = await this.parse(Deploy);
-
+    public async execute(): Promise<void> {
         // print machine hash
         const templateHash = this.getMachineHash();
         if (!templateHash) {
-            this.error(
-                `Cartesi machine snapshot not found, run '${this.config.bin} build'`,
+            throw new UsageError(
+                "Cartesi machine snapshot not found, run 'build'",
             );
         }
         this.logPrompt({
@@ -44,7 +41,7 @@ export default class Deploy extends BaseCommand<typeof Deploy> {
 
         // ask for deployment type
         const hosting =
-            flags.hosting ||
+            this.hosting ||
             (await select<"self-hosted" | "third-party">({
                 message: "Select hosting type",
                 choices: [
@@ -74,18 +71,20 @@ You will need the following infrastructure:
         switch (hosting) {
             case "self-hosted": {
                 // build docker image
-                await this.config.runCommand("deploy:build");
+                await this.cli.run(["deploy", "build"]);
 
                 queryString = `?templateHash=${templateHash}`;
                 break;
             }
             case "third-party": {
-                this.error("Third-party provider deployment not supported yet");
+                throw new UsageError(
+                    "Third-party provider deployment not supported yet",
+                );
             }
         }
 
         // prompt user to open webapp for onchain deployment
-        const deployUrl = `${flags.webapp}${queryString}`;
+        const deployUrl = `${this.webapp}${queryString}`;
         if (await confirm({ message: `Open ${chalk.cyan(deployUrl)}?` })) {
             open(deployUrl, { app: { name: apps.chrome } });
         }

apps/cli/src/commands/hash.ts
@@ -1,28 +1,30 @@
 import chalk from "chalk";
+import { Command, UsageError } from "clipanion";
 
 import { BaseCommand } from "../baseCommand.js";
 
-export default class HashCommand extends BaseCommand<typeof HashCommand> {
-    static summary = "Prints out image hash generated by the build command";
+export default class HashCommand extends BaseCommand {
+    static paths = [["hash"]];
 
-    static description =
-        "Converts the binary generated by the build command to hexadecimal and prints out the result to console";
+    static usage = Command.Usage({
+        description: "Prints out image hash generated by the build command",
+        details:
+            "Converts the binary generated by the build command to hexadecimal and prints out the result to console",
+    });
 
-    public static enableJsonFlag = true;
-
-    public async run() {
+    public async execute() {
         const hash = this.getMachineHash();
         if (hash) {
-            if (!this.jsonEnabled()) {
-                process.stdout.write(
+            if (!this.jsonEnabled) {
+                this.context.stdout.write(
                     `${chalk.green("?")} Cartesi machine templateHash ${chalk.cyan(hash)}\n`,
                 );
             } else {
-                return { hash };
+                this.context.stdout.write(JSON.stringify({ hash }));
             }
         } else {
-            this.error(
-                `Cartesi machine snapshot not found, run '${this.config.bin} build'`,
+            throw new UsageError(
+                "Cartesi machine snapshot not found, run 'build'",
             );
         }
     }

apps/cli/src/commands/send/dapp-address.ts
@@ -1,3 +1,4 @@
+import { Command } from "clipanion";
 import { Address, PublicClient, WalletClient } from "viem";
 
 import {
@@ -6,13 +7,13 @@ import {
 } from "../../contracts.js";
 import { SendBaseCommand } from "./index.js";
 
-export default class SendAddress extends SendBaseCommand<typeof SendAddress> {
-    static summary = "Send DApp address input to the application.";
+export default class SendAddressCommand extends SendBaseCommand {
+    static paths = [["send", "dapp-address"]];
 
-    static description =
-        "Sends an input to the application with its own address.";
-
-    static examples = ["<%= config.bin %> <%= command.id %>"];
+    static usage = Command.Usage({
+        description: "Send DApp address input to the application.",
+        details: "Sends an input to the application with its own address.",
+    });
 
     public async send(
         publicClient: PublicClient,

apps/cli/src/index.ts
@@ -1,1 +1,41 @@
-export { run } from "@oclif/core";
+import { Builtins, Cli } from "clipanion";
+
+import pkg from "../package.json";
+import AddressBookCommand from "./commands/address-book.js";
+import BuildCommand from "./commands/build.js";
+import CleanCommand from "./commands/clean.js";
+import CreateCommand from "./commands/create.js";
+import DeployBuildCommand from "./commands/deploy/build.js";
+import DeployCommand from "./commands/deploy/index.js";
+import DoctorCommand from "./commands/doctor.js";
+import HashCommand from "./commands/hash.js";
+import RunCommand from "./commands/run.js";
+import SendAddressCommand from "./commands/send/dapp-address.js";
+import SendCommand from "./commands/send/index.js";
+import ShellCommand from "./commands/shell.js";
+
+const [, , ...args] = process.argv;
+const cli = new Cli({
+    binaryName: "cartesi",
+    binaryLabel: "Cartesi",
+    binaryVersion: pkg.version,
+});
+
+cli.register(Builtins.DefinitionsCommand);
+cli.register(Builtins.HelpCommand);
+cli.register(Builtins.TokensCommand);
+cli.register(Builtins.VersionCommand);
+
+cli.register(AddressBookCommand);
+cli.register(BuildCommand);
+cli.register(CleanCommand);
+cli.register(CreateCommand);
+cli.register(DeployCommand);
+cli.register(DeployBuildCommand);
+cli.register(DoctorCommand);
+cli.register(HashCommand);
+cli.register(RunCommand);
+cli.register(SendCommand);
+cli.register(SendAddressCommand);
+cli.register(ShellCommand);
+cli.runExit(args);