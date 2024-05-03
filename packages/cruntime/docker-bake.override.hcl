sunodo/sunodo

feat(cruntime): add package

packages/cruntime/docker-bake.override.hcl
@@ -1,0 +1,3 @@
+target "default" {
+  tags = ["sunodo/cruntime:devel"]
+}

cartesi/cli

packages/cruntime/docker-bake.override.hcl
@@ -1,0 +1,3 @@
+target "default" {
+  tags = ["cartesi/cruntime:devel"]
+}