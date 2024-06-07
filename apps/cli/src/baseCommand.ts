8d5ae872bee51c01cda682c19d4680e773fe06ac
Danilo Tuler <tuler@pobox.com>
2024-06-06 22:43:10 -0300

    feat(cli): remove unused type

--- apps/cli/src/baseCommand.ts
+++ apps/cli/src/baseCommand.ts
@@ -26,14 +26,7 @@ export type Flags<T extends typeof Command> = Interfaces.InferredFlags<
     (typeof BaseCommand)["baseFlags"] & T["flags"]
 >;
 export type Args<T extends typeof Command> = Interfaces.InferredArgs<T["args"]>;
-
 export type AddressBook = Record<string, Address>;
-export interface DApp {
-    address: Address;
-    blockHash: Address;
-    blockNumber: number;
-    transactionHash: Address;
-}
 
 export abstract class BaseCommand<T extends typeof Command> extends Command {
     protected flags!: Flags<T>;
