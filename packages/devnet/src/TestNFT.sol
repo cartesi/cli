58b90ee7aa1d3b124f04277761495cf4daa8af19
Danilo Tuler <tuler@pobox.com>
2025-03-25 15:59:23 -0400

    chore(devnet): format code with forge fmt

--- packages/devnet/src/TestNFT.sol
+++ packages/devnet/src/TestNFT.sol
@@ -7,30 +7,20 @@ import "@openzeppelin-contracts-5.2.0/token/ERC721/extensions/ERC721URIStorage.s
 import "@openzeppelin-contracts-5.2.0/access/Ownable.sol";
 
 contract TestNFT is ERC721, ERC721URIStorage, Ownable {
-    constructor(
-        address initialOwner
-    ) ERC721("TestNFT", "SUNN") Ownable(initialOwner) {}
+    constructor(address initialOwner) ERC721("TestNFT", "SUNN") Ownable(initialOwner) {}
 
-    function safeMint(
-        address to,
-        uint256 tokenId,
-        string memory uri
-    ) public onlyOwner {
+    function safeMint(address to, uint256 tokenId, string memory uri) public onlyOwner {
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function tokenURI(
-        uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
