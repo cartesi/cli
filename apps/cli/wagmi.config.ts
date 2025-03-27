2050c9eff9d470553d8bb852a46571199a35c462
Danilo Tuler <tuler@pobox.com>
2025-03-27 13:17:56 -0400

    feat(cli): use devnet 2.0.0-alpha.5
    - rollups-contracts 2.0.0-rc.17
    - cannon chain 13370

--- apps/cli/wagmi.config.ts
+++ apps/cli/wagmi.config.ts
@@ -1,9 +1,81 @@
-import hardhatDeploy from "@sunodo/wagmi-plugin-hardhat-deploy";
-import { defineConfig } from "@wagmi/cli";
+import { defineConfig, type Plugin } from "@wagmi/cli";
+import { readdirSync, readFileSync } from "fs";
+import path from "path";
+
+const DataAvailability = JSON.parse(
+    readFileSync(
+        "node_modules/@cartesi/rollups/out/DataAvailability.sol/DataAvailability.json",
+        "utf8",
+    ),
+);
+
+interface CannonOptions {
+    directory: string;
+    includes?: RegExp[];
+    excludes?: RegExp[];
+}
+
+const shouldIncludeFile = (name: string, config: CannonOptions): boolean => {
+    if (config.excludes) {
+        // if there is a list of excludes, then if the name matches any of them, then exclude
+        for (const exclude of config.excludes) {
+            if (exclude.test(name)) {
+                return false;
+            }
+        }
+    }
+    if (config.includes) {
+        // if there is a list of includes, then only include if the name matches any of them
+        for (const include of config.includes) {
+            if (include.test(name)) {
+                return true;
+            }
+        }
+        return false;
+    } else {
+        // if there is no list of includes, then include everything
+        return true;
+    }
+};
+
+const cannonDeployments = (config: CannonOptions): Plugin => {
+    return {
+        name: "cannon",
+        contracts: () => {
+            // list all files exported by cannon in directory
+            const files = readdirSync(config.directory).filter((file) =>
+                shouldIncludeFile(file, config),
+            );
+
+            // return an array of contracts as expected by wagmi
+            return files.map((file) => {
+                // read the file and parse it as json
+                const deployment = JSON.parse(
+                    readFileSync(path.join(config.directory, file), "utf8"),
+                );
+
+                // get the address and abi from the deployment
+                return {
+                    abi: deployment.abi,
+                    address: deployment.address,
+                    name: deployment.contractName,
+                };
+            });
+        },
+    };
+};
 
 export default defineConfig({
     out: "src/contracts.ts",
+    contracts: [
+        {
+            name: "DataAvailability",
+            abi: DataAvailability.abi as any,
+        },
+    ],
     plugins: [
-        hardhatDeploy({ directory: "node_modules/@cartesi/devnet/export/abi" }),
+        cannonDeployments({
+            directory: "node_modules/@cartesi/devnet/deployments",
+        }),
     ],
 });
