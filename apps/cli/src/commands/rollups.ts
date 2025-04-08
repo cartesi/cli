86b1df96595a4c1706143a814cf4c4088d231775
Enderson Maia <endersonmaia@gmail.com>
2025-04-08 16:36:23 -0300

    feat(cli): add rollups create command

--- apps/cli/src/commands/rollups.ts
+++ apps/cli/src/commands/rollups.ts
@@ -1,4 +1,5 @@
 import { Command } from "@commander-js/extra-typings";
+import { createCreateCommand } from "./rollups/create.js";
 import { createDeployCommand } from "./rollups/deploy.js";
 import { createLogsCommand } from "./rollups/logs.js";
 import { createStartCommand } from "./rollups/start.js";
@@ -20,6 +21,7 @@ export const createRollupsCommand = () => {
     command.addCommand(createStatusCommand());
     command.addCommand(createStopCommand());
     command.addCommand(createDeployCommand());
+    command.addCommand(createCreateCommand());
     return command;
 };
 
