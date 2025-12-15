dfc0293ede8ddf1305fce0e19e7f29713398d52d
Danilo Tuler <tuler@pobox.com>
2025-12-15 14:16:05 -0500

    feat(devnet): bump openzeppelin

--- packages/devnet/src/TestMultiToken.sol
+++ packages/devnet/src/TestMultiToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
-// Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
+// Compatible with OpenZeppelin Contracts ^5.0
+pragma solidity ^0.8;
 
-import "@openzeppelin-contracts-5.2.0/token/ERC1155/ERC1155.sol";
-import "@openzeppelin-contracts-5.2.0/access/Ownable.sol";
+import {ERC1155} from "@openzeppelin-contracts-5.5.0/token/ERC1155/ERC1155.sol";
+import {Ownable} from "@openzeppelin-contracts-5.5.0/access/Ownable.sol";
 
 contract TestMultiToken is ERC1155, Ownable {
     constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {}
@@ -12,14 +12,21 @@ contract TestMultiToken is ERC1155, Ownable {
         _setURI(newuri);
     }
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyOwner {
         _mint(account, id, amount, data);
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-        onlyOwner
-    {
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyOwner {
         _mintBatch(to, ids, amounts, data);
     }
 }
--- packages/devnet/src/TestToken.sol
+++ packages/devnet/src/TestToken.sol
@@ -1,14 +1,31 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.20;
+// Compatible with OpenZeppelin Contracts ^5.0
+pragma solidity ^0.8;
 
-import "@openzeppelin-contracts-5.2.0/token/ERC20/ERC20.sol";
-import "@openzeppelin-contracts-5.2.0/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin-contracts-5.2.0/token/ERC20/extensions/ERC20Pausable.sol";
-import "@openzeppelin-contracts-5.2.0/access/manager/AccessManaged.sol";
-import "@openzeppelin-contracts-5.2.0/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20} from "@openzeppelin-contracts-5.5.0/token/ERC20/ERC20.sol";
+import {
+    ERC20Burnable
+} from "@openzeppelin-contracts-5.5.0/token/ERC20/extensions/ERC20Burnable.sol";
+import {
+    ERC20Pausable
+} from "@openzeppelin-contracts-5.5.0/token/ERC20/extensions/ERC20Pausable.sol";
+import {
+    AccessManaged
+} from "@openzeppelin-contracts-5.5.0/access/manager/AccessManaged.sol";
+import {
+    ERC20Permit
+} from "@openzeppelin-contracts-5.5.0/token/ERC20/extensions/ERC20Permit.sol";
 
-contract TestToken is ERC20, ERC20Burnable, ERC20Pausable, AccessManaged, ERC20Permit {
-    constructor(address initialAuthority)
+contract TestToken is
+    ERC20,
+    ERC20Burnable,
+    ERC20Pausable,
+    AccessManaged,
+    ERC20Permit
+{
+    constructor(
+        address initialAuthority
+    )
         ERC20("TestToken", "TEST")
         AccessManaged(initialAuthority)
         ERC20Permit("TestToken")
@@ -26,7 +43,11 @@ contract TestToken is ERC20, ERC20Burnable, ERC20Pausable, AccessManaged, ERC20P
 
     // The following functions are overrides required by Solidity.
 
-    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Pausable) {
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Pausable) {
         super._update(from, to, value);
     }
 }
